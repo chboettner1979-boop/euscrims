01f80c40e72eaa73b424d7a08b82e1a2d59ff92e
totytinasia-commits <totytinasia@gmail.com>
2026-08-12 19:48:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -58,10 +58,11 @@ with col2:
     
     # Pulsanti uno sopra l'altro
     if st.button("Player", use_container_width=True): st.session_state.page = 'Player'
+    if st.button("TEAMS", use_container_width=True): st.session_state.page = 'Teams'
     if st.button("Rules", use_container_width=True): st.session_state.page = 'Rules'
     if st.button("Scrims", use_container_width=True): st.session_state.page = 'Scrims'
     if st.button("Stats", use_container_width=True): st.session_state.page = 'Stats'
-    if st.button("TEAMS", use_container_width=True): st.session_state.page = 'Teams'
+    
 
     # DYNAMIC CONTENT
     if st.session_state.page == 'Rules':
