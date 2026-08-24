b3be1686f49c1119875607eaaabb7fe4d23a67a1
totytinasia-commits <totytinasia@gmail.com>
2026-08-24 18:24:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -326,7 +326,7 @@ with col2:
                         deadliest_d = format_val(h20_l21[1][3] if len(h20_l21[1]) > 3 else 0)
                         deadliest_a = format_val(h20_l21[1][4] if len(h20_l21[1]) > 4 else 0, is_percentage=True)
 
-                weapons_raw = target_ws.get("F27:L67")
+                weapons_raw = target_ws.get("F27:L68")
                 if weapons_raw:
                     for r_data in weapons_raw:
                         if r_data and len(r_data) > 0:
